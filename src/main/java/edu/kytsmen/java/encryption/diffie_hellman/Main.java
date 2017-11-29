017e6b198c0575b1b4da1785cefdaf47e6c0ab08
Dmytro Kytsmen <dmytro.kytsmen@cloudmade.com>
2017-11-29 10:57:55 +0200

    Added implementation for Diffie-Hellman protocol

--- src/main/java/edu/kytsmen/java/encryption/diffie_hellman/Main.java
+++ src/main/java/edu/kytsmen/java/encryption/diffie_hellman/Main.java
@@ -1,4 +1,21 @@
 package edu.kytsmen.java.encryption.diffie_hellman;
 
 public class Main {
+    public static void main(String[] args) {
+        final Person jane = new Person();
+        final Person john = new Person();
+
+        jane.generateKeys();
+        john.generateKeys();
+
+        jane.receivePublicKeyFrom(john);
+        john.receivePublicKeyFrom(jane);
+
+        jane.generateCommonSecretKey();
+        john.generateCommonSecretKey();
+
+        jane.encryptAndSendMessage("Guess who I am?", john);
+
+        john.whisperTheSecretMessage();
+    }
 }
